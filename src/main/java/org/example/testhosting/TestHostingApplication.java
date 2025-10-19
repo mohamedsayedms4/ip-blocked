95e9048121f78250b7cf855a1e186f005718f15c
mohamedsayedms4 <mohamedms4002@gmail.com>
2025-10-19 04:40:13 +0300

    first commit

---
+++ src/main/java/org/example/testhosting/TestHostingApplication.java
@@ -0,0 +1,13 @@
+package org.example.testhosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHostingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHostingApplication.class, args);
+    }
+
+}
